d4b8dfe18a35d38d424c0b3271e38dd4a0e311ef
rustinchen <t18kc22@abdn.ac.uk>
2023-03-03 03:04:19 +0000

    PUSH

--- setup_data.py
+++ setup_data.py
@@ -8,10 +8,14 @@ cur = conn.cursor()
 # drop the data from the table so that if we rerun the file, we don't repeat values
 conn.execute('DROP TABLE IF EXISTS IMDb_All_Genres_etf_clean1')
 print("table dropped successfully");
+conn.execute('DROP TABLE IF EXISTS Director')
+print("table dropped successfully");
 
 # create table
 cur.execute('''CREATE TABLE IMDb_All_Genres_etf_clean1
                (Movie_Title TEXT, Year TEXT, Director TEXT, Rating TEXT, Runtime TEXT, Censor TEXT, Total_Gross TEXT, main_genre TEXT)''')
+cur.execute('''CREATE TABLE Director
+               (Director TEXT, Time_span TEXT, Total_Money TEXT, Highest_rating TEXT)''')
 
 # open the file to read it into the database
 with open('IMDb_movies_director/IMDb_All_Genres_etf_clean1.csv', 'r', newline='') as f:
@@ -30,6 +34,19 @@ with open('IMDb_movies_director/IMDb_All_Genres_etf_clean1.csv', 'r', newline=''
         main_genre = row[7]
 
         cur.execute('INSERT INTO IMDb_All_Genres_etf_clean1 VALUES (?,?,?,?,?,?,?,?)', (Movie_Title, Year, Director, Rating, Runtime, Censor, Total_Gross, main_genre))
+        conn.commit()
+
+with open('IMDb_movies_director/Director.csv', 'r', newline='') as f:
+    reader = csv.reader(f, delimiter=",")
+    next(reader) # skip the header line
+    for row in reader:
+        print(row)
+        Director = row[0]
+        Time_span = row[1]
+        Total_Money = row[2]
+        Highest_rating = row[3]
+
+        cur.execute('INSERT INTO Director VALUES (?,?,?,?,?,?,?,?)', (Director, Time_span, Total_Money, Highest_rating))
         conn.commit()
 
 print("data parsed successfully")
