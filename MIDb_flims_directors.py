2e9e1ad22696da93d43928f0bf7b15c6ef7cb056
rustinchen <t18kc22@abdn.ac.uk>
2023-03-03 00:17:23 +0000

    Initial commit

---
+++ MIDb_flims_directors.py
@@ -0,0 +1,38 @@
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# database details - to remove some duplication
+db_name = 'MIDb_films_directors.db'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/films')
+def films():
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    # get results from films
+    cur.execute("select * from films")
+    rows = cur.fetchall()
+    conn.close()
+    return render_template('films.html', rows=rows)
+
+@app.route('/directors')
+def directors():
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    # get results from directors
+    cur.execute("select * from directors")
+    rows = cur.fetchall()
+    conn.close()
+    return render_template('directors.html', rows=rows)
+
+
+
+
+
